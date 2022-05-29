95bfbc318469a40da4d6f2f871f441a9e104928c
Yao Yuan <yaoyuanyy@outlook.com>
2022-05-29 11:49:58 +0800

    feat: sync api (#3)

--- src/main/kotlin/com/example/demo/anki/Action.kt
+++ src/main/kotlin/com/example/demo/anki/Action.kt
@@ -1,6 +1,9 @@
 package com.example.demo.anki
 
+import com.fasterxml.jackson.annotation.JsonInclude
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class Action(
     val action: String,
-    val params: Params?
+    val params: Params? = null
 )
