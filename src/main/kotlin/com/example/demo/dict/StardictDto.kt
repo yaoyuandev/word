dcbe0a9a3adbeba4f5e1ecb0194e21037ff27b29
Yao Yuan <yaoyuanyy@outlook.com>
2023-02-15 15:19:24 +0800

    add some links

--- src/main/kotlin/com/example/demo/dict/StardictDto.kt
+++ src/main/kotlin/com/example/demo/dict/StardictDto.kt
@@ -11,7 +11,9 @@ data class StardictDto(
     val translation: String? = null,
     val exchange: String? = null,
     val links: ArrayList<String> = arrayListOf("https://www.vocabulary.com/dictionary/$word",
-        "https://www.etymonline.com/word/$word")
+        "https://www.etymonline.com/word/$word", "https://www.merriam-webster.com/dictionary/$word",
+    "http://dict.cn/$word","https://youdao.com/result?word=$word&lang=en"
+        )
 ) : Serializable {
 
     fun toNote() = Note(
